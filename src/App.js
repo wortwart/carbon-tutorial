07df43b3c68dd34a719408f1e53276d8886de650
Herbert Braun <herbert.braun@ibmix.de>
2023-09-19 15:44:31 +0200

    Tutorial level 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import {Route, Routes} from 'react-router-dom';
+import {Content, Theme} from '@carbon/react';
+import TutorialHeader from './components/TutorialHeader';
+import LandingPage from './content/LandingPage';
+import RepoPage from './content/RepoPage';
+import './app.scss';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello Carbon! Well, not quite yet. This is the starting point for the
-        Carbon tutorial.
-      </div>
-    );
-  }
+	render() {
+		return (
+			<>
+				<Theme theme="g100">
+					<TutorialHeader/>
+				</Theme>
+				<Content>
+					<Routes>
+						<Route exact path="/" element={<LandingPage/>}/>
+						<Route path="/repos" element={<RepoPage/>}/>
+					</Routes>
+				</Content>
+			</>
+		);
+	}
 }
 
 export default App;
